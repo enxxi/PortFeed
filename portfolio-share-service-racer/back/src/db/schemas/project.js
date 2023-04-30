7b04a026e2df274a30c94a0fa4b602553ca33a08
[AI7}탁은경 <xkrdmsrud24@gmail.com>
2023-04-30 19:46:20 +0900

    project-mvp-back clean, modify-라우터 경로 수정

---
+++ portfolio-share-service-racer/back/src/db/schemas/project.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const ProjectSchema = new Schema(
+  {
+    user_id: {
+      type: String,
+      required: true,
+    },
+    project_id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      default: "포트폴리오 상세설명을 추가해주세요.",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProjectModel = model("Project", ProjectSchema);
+
+export { ProjectModel }
