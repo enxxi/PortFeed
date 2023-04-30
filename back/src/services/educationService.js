f5c03528cf1ba11f025873590c6a51d7047cb9b1
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-04-30 15:56:10 +0900

    modify - edu->education 수정, edu CRUD 완성(path 수정)

--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -3,10 +3,10 @@ import { v4 as uuidv4 } from "uuid";
 
 class EducationService {
   // 학력을 추가하는 함수
-  static async addEducation({ user_id, name, field }) {
+  static async addEducation({ user_id, school, major, degree }) {
     // id는 유니크 값 부여
-    const id = uuidv4();
-    const newEducation = { id, user_id, name, field };
+    const education_id = uuidv4();
+    const newEducation = { education_id, user_id, school, major, degree };
 
     //db에 저장
     const createdNewEducation = await Education.create({ newEducation });
@@ -14,10 +14,10 @@ class EducationService {
     return createdNewEducation;
   }
 
-  // edu_id에 해당하는 학력을 가져오는 함수
-  static async getEducation({ edu_id }) {
+  // education_id에 해당하는 학력을 가져오는 함수
+  static async getEducation({ education_id }) {
     //edu db에 존재 여부 확인
-    const education = await Education.findById({ edu_id });
+    const education = await Education.findById({ education_id });
     if (!education) {
       const errorMessage =
         "해당 id를 가진 학력은 없습니다. 다시 한 번 확인해 주세요.";
@@ -34,8 +34,8 @@ class EducationService {
   }
 
   //해당 id의 학력을 수정하는 함수
-  static async setEducation({ edu_id, toUpdate }) {
-    let education = await Education.findById({ edu_id });
+  static async setEducation({ education_id, toUpdate }) {
+    let education = await Education.findById({ education_id });
 
     //db에서 찾지 못한 경우, 에러 메시지 반환
     if (!education) {
@@ -45,23 +45,41 @@ class EducationService {
     }
 
     //업데이트 대상의 값이 null이 아니라면 업데이트
-    if (toUpdate.name) {
-      const fieldToUpdate = "name";
-      const newValue = toUpdate.name;
-      education = await Education.update({ edu_id, fieldToUpdate, newValue });
+    if (toUpdate.school) {
+      const fieldToUpdate = "school";
+      const newValue = toUpdate.school;
+      education = await Education.update({
+        education_id,
+        fieldToUpdate,
+        newValue,
+      });
     }
 
-    if (toUpdate.field) {
-      const fieldToUpdate = "field";
-      const newValue = toUpdate.field;
-      education = await Education.update({ edu_id, fieldToUpdate, newValue });
+    if (toUpdate.major) {
+      const fieldToUpdate = "major";
+      const newValue = toUpdate.major;
+      education = await Education.update({
+        education_id,
+        fieldToUpdate,
+        newValue,
+      });
+    }
+
+    if (toUpdate.degree) {
+      const fieldToUpdate = "degree";
+      const newValue = toUpdate.degree;
+      education = await Education.update({
+        education_id,
+        fieldToUpdate,
+        newValue,
+      });
     }
 
     return education;
   }
 
-  static async deleteEducation({ edu_id }) {
-    const isDataDeleted = await Education.deleteById({ edu_id });
+  static async deleteEducation({ education_id }) {
+    const isDataDeleted = await Education.deleteById({ education_id });
 
     if (!isDataDeleted) {
       const errorMessage =
